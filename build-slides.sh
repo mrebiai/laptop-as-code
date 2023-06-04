9fbd2a6fafab77434c2ecd5980ed65288f5da9a3
Mehdi Rebiai <mehdi.rebiai.dev@gmail.com>
2023-06-04 23:45:39 +0200

    feat: css

--- build-slides.sh
+++ build-slides.sh
@@ -4,11 +4,12 @@ docker run --rm -u $(id -u):$(id -g) -v $(pwd):/documents asciidoctor/docker-asc
   asciidoctor-revealjs -a data-uri -a revealjs_theme=white \
   -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 -a revealjs_transition=fade \
   -a revealjs_slideNumber=true \
+  -a customcss=custom.css \
   -D public -o index.html \
   presentation.adoc
 
-# -a customcss=custom.css  
-
-#cp custom.css public/
+CONFERENCE_NAME="bordeaux-jug"
+CONFERENCE_PNG_BASE64=$(cat images/logo-${CONFERENCE_NAME}.png | base64 -w0) QRCODE_PNG_BASE64=$(cat images/qrcode.png | base64 -w0) \
+  envsubst < custom.css > public/custom.css 
 
 touch public/.nojekyll
